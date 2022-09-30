d8d0b83dee5f9ac37bc49d8b61c760a57d98b758
acliya <114714403+acliya@users.noreply.github.com>
2022-09-30 16:12:03 +0800

    Update request-body-test.js

--- request-body-test.js
+++ request-body-test.js
@@ -2,7 +2,7 @@
 访问 http://httpbin.org/forms/post 提交表单进行测试。
 
 [rewrite_local]
-http:\/\/httpbin\.org\/post url script-response-body https://raw.githubusercontent.com/acliya/test/main/request-body-test.js
+http:\/\/httpbin\.org\/post url script-request-body https://raw.githubusercontent.com/acliya/test/main/request-body-test.js
 */
 
 const responseBody = $response.body;
@@ -11,6 +11,6 @@ const requestBody = $request.body;
 console.log('请求数据：', requestBody);
 console.log('响应数据：', responseBody);
 
-$notify('重定测试', '测试获取请求体', `# 请求数据\n${requestBody}\n\n# 响应数据：\n${responseBody}`);
+$notify('重写测试', '测试获取请求体', `# 请求数据\n${requestBody}\n\n# 响应数据：\n${responseBody}`);
 
 $done($response.body);
