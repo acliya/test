d9ede5e38833a9ee655efc57fd1c5bdcf7e8dcea
acliya <114714403+acliya@users.noreply.github.com>
2022-09-30 16:14:54 +0800

    Update request-body-test.js

--- request-body-test.js
+++ request-body-test.js
@@ -5,12 +5,17 @@
 http:\/\/httpbin\.org\/post url script-request-body https://raw.githubusercontent.com/acliya/test/main/request-body-test.js
 */
 
-const responseBody = $response.body;
+
 const requestBody = $request.body;
-
 console.log('请求数据：', requestBody);
-console.log('响应数据：', responseBody);
+$notify('重写测试', '测试获取请求体', `# 请求数据\n${requestBody}`);
 
-$notify('重写测试', '测试获取请求体', `# 请求数据\n${requestBody}\n\n# 响应数据：\n${responseBody}`);
+if($response != null) {
+  const responseBody = $response.body;
+  console.log('响应数据：', responseBody);
+  
+  $notify('重写测试', '测试获取响应体', `# 响应数据\n${responseBody}`);
+}
+
 
 $done($response.body);
